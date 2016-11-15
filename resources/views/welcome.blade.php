767ce47c1dee874b001460e3c2175e0465d241a4
David Tinnes <David.Tinnes@dowjones.com>
2016-11-15 15:35:05 -0600

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 222
                 </div>
 
                 <div class="links">
